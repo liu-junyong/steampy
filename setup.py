1244760d060520a45965401bf21a4aa11c720648
liu-junyong <720556@qq.com>
2022-06-09 11:24:40 +0800

    合并

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ import sys
 if not sys.version_info[0] == 3 and sys.version_info[1] < 5:
     sys.exit('Python < 3.5 is not supported')
 
-version = '0.75'
+version = '0.76a'
 
 setup(
     name='steampy',
     packages=['steampy', 'test', 'examples', ],
     version=version,
-    description='A Steam lib for trade automation',
-    author='Michał Bukowski',
-    author_email='gigibukson@gmail.com',
+    description='A Steam lib for trade automation,fork from :https://github.com/bukson/steampy',
+    author='liujunyong,origin author:bukson',
+    author_email='liu.junyong@gmail.com',
     license='MIT',
-    url='https://github.com/bukson/steampy',
-    download_url='https://github.com/bukson/steampy/tarball/' + version,
+    url='https://gitee.com/liu_junyong/steampy',
+    download_url='https://gitee.com/liu_junyong/steampy/' + version,
     keywords=['steam', 'trade', ],
     classifiers=[],
     install_requires=[
